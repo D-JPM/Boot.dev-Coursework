11bb18efa58d1a4556daea4f48aef258da21dd70
D-JPM <DeclanJPMetcalfe@protonmail.com>
2025-04-06 23:25:44 +0100

    Add check to run main function only when script is executed directly

--- bookbot/main.py
+++ bookbot/main.py
@@ -5,10 +5,12 @@ def main():
         file_contents = f.read() #Access f which is the novel and read it.
     
     def get_nums_words(text):
-        words - text.split() #Split the text into words by whitespace.
+        words = text.split() #Split the text into words by whitespace.
         return len(words) #Return the number of words.
 
-    num_words = get_num_words(file_contents) #Passing file_contents to get num words and storing inside num words.
+    num_words = get_nums_words(file_contents) #Passing file_contents to get num words and storing inside num words.
 
     print(f"--- Begin report of {f.name} ---") #Extracting name of novel from file object.
-     
+
+if __name__ == "__main__": # If this script is being run directly (not imported), execute the main function.
+    main()
