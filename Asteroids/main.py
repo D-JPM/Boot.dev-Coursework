f2c7ed5276b4a95ab66bcc6437a52d41b5de94d9
D-JPM <declanjpmetcalfe@protonmail.com>
2025-10-26 19:12:46 +0000

    Remove Asteroids project

--- Asteroids/main.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-def main():
-    print("Starting asteroids!")
-
-# This line ensures the 'main()' function is only called when this file is run directly.
-# It will not run if it is imported as a module.
-if __name__ == "__main__":
-    main()
