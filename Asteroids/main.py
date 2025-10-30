74aa564b4353c914d60ab2f746152ddee0e31415
D-JPM <declanjpmetcalfe@protonmail.com>
2025-10-30 18:55:27 +0000

    Add player.py - Render Player

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     print("Starting Asteroids!")
@@ -17,19 +18,26 @@ def main():
     black = (0, 0 , 0) # Solid black colour
     screen.fill(black) # Fill screen with colour black
 
-    # Game Loop
+        # Instantiate Player
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+        # Game Loop
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                running = False
 
         # Game state update
         dt = clock.tick(60) / 1000.0
         
+        screen.fill("black")          # clear
+        # test circle at exact center
+        #pygame.draw.circle(screen, "yellow", (640, 360), PLAYER_RADIUS, 1)
+        player.draw(screen)           # render player
         # Display updates
-        pygame.display.flip()
+        pygame.display.flip()         # present
 
-
+        
 if __name__ == "__main__":
     main()
